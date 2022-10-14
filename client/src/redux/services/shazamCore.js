b310a56ab7e6f631bb80efc1013b92237812ef1a
zeineb-sahli <zeinebbsahli@gmail.com>
2022-10-14 22:55:34 +0100

    initial client commit

---
+++ client/src/redux/services/shazamCore.js
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '40173bcd2amshe8c860d81638fa2p17cdddjsn9cc5542c86f2')
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+        getSongsByGenre: builder.query({ query:(genre)=> `/charts/genre-world?genre_code=${genre}`}),
+        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+        getSongsBySearch :builder.query({query:(searchTerm) =>`/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`}),
+    }),
+
+});
+export const {
+    useGetTopChartsQuery,
+    useGetSongsBySearchQuery,
+    useGetSongsByGenreQuery,
+    useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
+    useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery,
+
+} = shazamCoreApi;
